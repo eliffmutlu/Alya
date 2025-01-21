d06f38c3d849dadf62d0e9511f3d3284f00ff23c
eliffmutlu <elifmutlu174@gmail.com>
2025-01-21 17:06:40 +0300

    Fix MIME type issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Base yolunu göreli olarak ayarla
+  base: '/', // Kök yol üzerinden servis edilmeli
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
